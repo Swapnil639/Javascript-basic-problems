61d796e742a0636ba8cc9fcd37dab5ae8ddf532c
Swapnil Patil <102506228+Swapnil639@users.noreply.github.com>
2022-10-03 19:34:52 +0530

    ADDED: Fliped coin to get heads or tails

---
+++ FlipCoin.js
@@ -0,0 +1,9 @@
+let head = 0;
+let tails = 0;
+let randomNumber = 0.0;
+randomNumber = Math.random();
+if(randomNumber<0.5){
+    console.log("On coin flip HEADS appeared");
+}else{
+    console.log("On coin flip TAILS appeared");
+}
