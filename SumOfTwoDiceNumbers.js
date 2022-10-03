2ea985d93dbe8abe92b3cc72dd0303e72767726a
Swapnil Patil <102506228+Swapnil639@users.noreply.github.com>
2022-10-03 19:55:41 +0530

    ADDED: Found sum of two random dice numbers

---
+++ SumOfTwoDiceNumbers.js
@@ -0,0 +1,7 @@
+let diceNumber1 = Math.floor(Math.random() * 6) +1;
+let diceNumber2 = Math.floor(Math.random() * 6) +1;
+let sum = diceNumber1 + diceNumber2;
+console.log("The two random dice number's are:");
+console.log(diceNumber1,diceNumber2);
+console.log("The sum of two random dice number is:");
+console.log(sum);
